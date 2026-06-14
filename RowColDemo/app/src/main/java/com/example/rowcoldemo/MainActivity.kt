e6258bf6a6b47c60578f71f0cd89adb64dec08e3
Niki <nik.mixa@mail.ru>
2026-06-14 22:13:18 +0700

    5.2

--- RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -48,7 +48,8 @@ fun GreetingPreview() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(modifier.size(width = 400.dp, height = 200.dp)) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.size(width = 400.dp, height = 200.dp)) {
         TextCell("1")
         TextCell("2")
         TextCell("3")
