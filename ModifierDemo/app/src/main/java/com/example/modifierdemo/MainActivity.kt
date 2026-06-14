4ce39060571d13f7481ceecee67b99e8da0cf973
Niki <nik.mixa@mail.ru>
2026-06-14 21:51:19 +0700

    7 Final

--- ModifierDemo/app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ ModifierDemo/app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -43,9 +43,11 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
-    val mymodifier = modifier
+    val mymodifier = Modifier
         .border(width = 2.dp, color = Color.Black)
         .padding(all = 10.dp)
+    val secondModifier = Modifier.height(100.dp)
+
     Column(
         Modifier.padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -53,7 +55,7 @@ fun DemoScreen(modifier: Modifier = Modifier) {
     ) {
         Text(
             "Hello Compose",
-            mymodifier,
+            mymodifier.then(secondModifier),
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold
         )
