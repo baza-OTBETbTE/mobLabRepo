7508ebca6021fad7881e41d36fa6fa5f2627b879
Niki <nik.mixa@mail.ru>
2026-06-15 05:51:10 +0700

    10

--- RoomDemo/app/build.gradle.kts
+++ RoomDemo/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.lifecycle.runtime.ktx)
