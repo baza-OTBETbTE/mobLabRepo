26bb2290a07be1ed1cb247621fd03dd46374925d
Niki <nik.mixa@mail.ru>
2026-06-15 04:43:09 +0700

    Верстка приложения

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.geoquiz.ui.theme.GeoQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GeoQuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    GeoQuizScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +47,80 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GeoQuizPreview() {
     GeoQuizTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            GeoQuizScreen(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GeoQuizScreen(modifier: Modifier, viewModel: GeoQuizViewModel = viewModel()) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+
+
+
+    Column() {
+        TopAppBar(
+            title = { Text("GeoQuiz", fontSize = 22.sp) },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFF3D35B1),
+                titleContentColor = Color.White,
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            modifier.padding(vertical = 15.dp).fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Canberra is the capital of Australia",
+                color = Color(0xFF4F4F4F)
+            )
+        }
+
+        Row(
+            modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = {},
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.padding(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color(0xFFFFFFFF),
+                    containerColor = Color(0xFF3D35B1))
+            ){ Text("TRUE", fontSize = 13.sp) }
+            Button(onClick = {},
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.padding(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color(0xFFFFFFFF),
+                    containerColor = Color(0xFF3D35B1))
+            ){ Text("FALSE", fontSize = 13.sp) }
+        }
+
+        Row(
+            modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Button(onClick = {},
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.padding(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color(0xFFFFFFFF),
+                    containerColor = Color(0xFF3D35B1))
+            ){ Text("NEXT >", fontSize = 13.sp) }
+        }
+    }
+
+
 }
