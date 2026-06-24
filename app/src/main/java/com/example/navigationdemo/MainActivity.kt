a88263af2b104005bc1b6f9bd09aa4c77b5b6491
Niki <nik.mixa@mail.ru>
2026-06-24 22:22:47 +0700

    9. Final

--- app/src/main/java/com/example/navigationdemo/MainActivity.kt
+++ app/src/main/java/com/example/navigationdemo/MainActivity.kt
@@ -49,7 +49,11 @@ fun MainScreen(modifier: Modifier = Modifier) {
     }
     NavDisplay(
         backStack = backStack,
-        onBack = { backStack.removeLastOrNull() },
+        onBack = {
+            while (backStack.size > 1) {
+                backStack.removeLastOrNull()
+            }
+        },
         entryProvider = entryProvider {
             entry<HomeScreen> {
                 Home(onNavigation)
