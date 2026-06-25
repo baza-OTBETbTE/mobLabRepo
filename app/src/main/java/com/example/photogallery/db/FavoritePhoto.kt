3d641d63ad7b1e44a2e33472cb9b705cdbaa82ad
Niki <nik.mixa@mail.ru>
2026-06-25 07:41:40 +0700

    3. DB Конфигурация, DAO

---
+++ app/src/main/java/com/example/photogallery/db/FavoritePhoto.kt
@@ -0,0 +1,34 @@
+package com.example.photogallery.db
+
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.flow.Flow
+
+@Entity(tableName = "favorites")
+data class FavoritePhoto(
+    @PrimaryKey val id: String,
+    val url: String
+)
+
+@Dao
+interface FavoritePhotoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(photo: FavoritePhoto)
+
+    @Query("SELECT * FROM favorites")
+    fun getAll(): Flow<List<FavoritePhoto>>
+
+    @Query("DELETE FROM favorites")
+    suspend fun deleteAll()
+}
+
+@Database(entities = [FavoritePhoto::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun favoritePhotoDao(): FavoritePhotoDao
+}
